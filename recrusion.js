dde1243cd089dfefc614edb03f13f27be718f5d3
satishshinde2048 <satishshinde1172000@gmail.com>
2022-11-26 22:13:35 +0530

    yes

--- recrusion.js
+++ recrusion.js
@@ -1,8 +1,17 @@
 function countdown(num){
-console.log(num);
+   // console.log(num);
+    
 let num1=num-1;
-if(num1>0){
+if(num1>=0){
+    if(num1==0){
+        console.log('Done');
+    }
+    else{
     countdown(num1);
+}}
+else{
+    console.log('invalid');
 }
-}
-countdown(10);
+    }
+
+countdown(4);
